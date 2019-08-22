1c708505092c320dac6c4a57f4de27eb6b0a4dc1
Administrateur <Administrateur@10.210.15.59>
2019-08-22 12:31:51 +0200

    Philippe : Avancement sur gain partie

--- JackBlack/src/main/java/greyjack/MoteurDeJeu.java
+++ JackBlack/src/main/java/greyjack/MoteurDeJeu.java
@@ -6,53 +6,6 @@ import java.io.InputStreamReader;
 
 public class MoteurDeJeu {
 	public static void main ( String[] args ) {
-		Sabot sabot = new Sabot ( 2 ) ;
-		sabot.melanger ( ) ;
-		
-		Croupier croupier = new Croupier ( 300000 ) ;
-		Joueur joueur = new Joueur ( 500 ) ;
-		joueur.tirerCarte ( sabot ) ;
-		croupier.tirerCarte ( sabot ) ;
-		joueur.tirerCarte ( sabot ) ;
-		
-		//Alexia.executerTour ( sabot ) ;
-		//sabot.afficherToutesLesCartesNoms ( ) ;
-
-		while ( ! joueur.getServi ( ) )
-		{
-			System.out.println ( ) ;
-			System.out.println ( ) ;
-			croupier.afficherCartes ( ) ;
-			croupier.afficherTotal ( ) ;
-			System.out.println ( "-------------------------------------------------------" ) ;
-			joueur.afficherCartes ( ) ;
-			joueur.afficherTotal ( ) ;
-			joueur.decider ( sabot ) ;
-		}
-		while ( ! croupier.getServi ( ) )
-		{
-			try {
-				Thread.sleep ( 500 ) ;
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			croupier.decider ( sabot ) ;
-			System.out.println ( ) ;
-			System.out.println ( ) ;
-			croupier.afficherCartes ( ) ;
-			croupier.afficherTotal ( ) ;
-			System.out.println ( "-------------------------------------------------------" ) ;
-			joueur.afficherCartes ( ) ;
-			joueur.afficherTotal ( ) ;
-		}
+		Manche manche = new Manche();
 	}
 }
-
-/*
-Classes a faire :
-- jeu de cartes
-- carte
-- Croupier
-- joueur
-
-*/
